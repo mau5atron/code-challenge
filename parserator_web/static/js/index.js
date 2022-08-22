784bf6b8e529ec38cc49fb5a0fe27f52be000d4e
mau5atron <gabrielbetancourt96@outlook.com>
2022-08-22 16:39:33 -0400

    Completed steps 1-4 as part of the code challenge - Wrote JS to pull data from the /api/parse endpoint - Displayed errors in the form of tooltips for parsing errors and empty input - /api/parse responds with JSON by handling query param '?input_string=' - Not familar with Django, Python, or testing in Django in general, so failed step 5 ( but I tried :) )

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,73 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+
+async function submitData(input_string){
+   // Using this function to make a get request at the specific API uri
+   // @ /api/parse and sending form input
+   const baseURI = "http://"+window.location.host;
+   const apiPath = "/api/parse/?input_string=";
+   const url = baseURI+apiPath+input_string;
+   const respose = await fetch(url, {
+      method: "GET",
+      headers: {
+         "Accept": "application/json",
+         "Content-Type": "application/json"
+      }
+   });
+
+   let data = await respose.json();
+   return data;
+}
+
+window.onload = () => {
+   document.querySelector("button#submit").addEventListener('click', (event) => {
+      event.preventDefault();   
+      const input_string = document.querySelector("input[name='address']");
+      const errorTooltip = document.getElementById("error-handle");
+      const addressType = document.getElementById("parse-type");
+      submitData(input_string.value)
+         .then(res => {
+            let tbody = document.querySelector("table tbody");
+            let addressResults = document.querySelector("div#address-results");
+            // if address obj does not exist, setup to display errors
+            if (res.address_components === undefined){
+               addressResults.style.display = 'none';
+               addressType.textContent = '';
+               tbody.textContent = '';
+               throw new Error(res.error);
+            }
+            
+            if (input_string.classList.contains("is-invalid") && errorTooltip.classList.contains("invalid-tooltip")){
+               input_string.classList.remove("is-invalid");
+               errorTooltip.classList.remove("invalid-tooltip");
+               errorTooltip.textContent = '';
+            }
+            // flush child nodes from tbody
+            if (addressResults.style.display == 'block') {
+               tbody.textContent = '';
+               addressType.textContent = '';
+            }
+            
+            addressType.textContent = res.address_type;
+            // setting up child nodes to be pushed into the data table
+            for (let prop in res.address_components){
+               let trow = document.createElement("tr");
+               let addressPartTd = document.createElement("td");
+               let tagTd = document.createElement("td");
+               let address_part_text = document.createTextNode(`${res.address_components[prop]}`);
+               let tag_text = document.createTextNode(`${prop}`);
+               addressPartTd.appendChild(address_part_text);
+               tagTd.appendChild(tag_text);
+               trow.appendChild(addressPartTd);
+               trow.appendChild(tagTd);
+               tbody.appendChild(trow);
+            }
+
+            addressResults.style.display = 'block';
+         }).catch(err => {
+            input_string.classList.add("is-invalid");
+            errorTooltip.classList.add("invalid-tooltip");
+            errorTooltip.textContent = err;
+         });
+   });
+} 
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -1,10 +1,12 @@
+from logging import exception
 import usaddress
 from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
-
+from django.http import JsonResponse
+from collections import OrderedDict    
 
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
@@ -16,9 +18,21 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        if request.method == 'GET':
+            input_string = request.GET.get('input_string')
+            print("string length: ")
+            print(len(input_string))
+            try:
+                address_components, address_type = self.parse(address=input_string)
+                if not input_string:
+                    raise ValueError("empty_input")
+            except usaddress.RepeatedLabelError:
+                return JsonResponse({"error": "Error handling input string"})
+            except ValueError:
+                return JsonResponse({"error": "Input cannot be empty"})
+            return JsonResponse({"address_components":address_components, "address_type":address_type})
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
-        return address_components, address_type
+        return usaddress.tag(address)
